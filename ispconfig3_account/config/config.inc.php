20dae614d3e99a42c9ce621e1fbe85d24a778de0
Leandro Noskoski <leandro@alternativalinux.net>
2024-05-15 14:58:53 -0300

    hide sensible data

--- ispconfig3_account/config/config.inc.php
+++ ispconfig3_account/config/config.inc.php
@@ -1,9 +1,4 @@
 <?php
-#$config['identity_limit'] = false;
-#$config['remote_soap_user'] = 'roundcube';
-#$config['remote_soap_pass'] = 'Bous_SQgR6';
-#$config['soap_url'] = 'https://painel.fecammx.com.br:8080/remote/';
-#$config['soap_validate_cert'] = true;
 $config['identity_limit'] = false;
 $config['remote_soap_user'] = $_ENV["REMOTE_SOAP_USER"];
 $config['remote_soap_pass'] = $_ENV["REMOTE_SOAP_PASS"];
