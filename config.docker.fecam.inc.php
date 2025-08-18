79d3e99e07eeed9d3c977ea3417ae6f903930664
Leandro Noskoski <leandro@alternativalinux.net>
2025-08-18 16:52:56 -0300

    cosmetics

--- config.docker.fecam.inc.php
+++ config.docker.fecam.inc.php
@@ -1,49 +1,39 @@
 <?php
 // Sessão do usuário (em minutos)
 ini_set('max_execution_time', 120);
-
 // ===== Timeouts de rede =====
 $config['imap_timeout'] = 60;   // segundos para operações IMAP
 $config['smtp_timeout'] = 60;   // segundos para operações SMTP
-
-// ===== Sessão (logout por inatividade) =====
-$config['session_lifetime'] = 1440 ; // minutos até expirar sessão (cookies)
+// Se tiver plugins que buscam conteúdo externo (URL), dê um timeout global:
+$config['http_client'] = ['timeout' => 10]; // via Guzzle
+$config['enable_spellcheck'] = false;
+$config['imap_conn_options'] = array(
+ 'ssl'         => array(
+    'verify_peer'  => false,
+     'verify_peer_name'  => false
+   )
+);
+$config['smtp_conn_options'] = array(
+ 'ssl'         => array(
+    'verify_peer'  => false,
+    'verify_peer_name'  => false
+  )
+);
+$config['max_message_size'] = 70000000;  // ~70MB para garantir 50MB de anexos
+$config['log_driver'] = 'file';
+$config['log_driver'] = 'stdout';
+$config['log_logins'] = true;  // Registra logins
+$config['language'] = 'pt_BR';
+$config['htmleditor'] = 1;
+$config['reply_mode'] = 1;
+$config['session_lifetime'] = 2880;
+$config['reconnect_imap_max_attempts'] = 3;
 $config['session_timeout']  = 720;  // minutos de inatividade antes do logout forçado
-
 // ===== Keep-alive/AJAX =====
 $config['keep_alive'] = 60;         // ping ao servidor para manter sessão/IMAP vivos
 $config['min_refresh_interval'] = 5; // intervalo mínimo entre atualizações automáticas (min)
-
 // (opcional) reduzir custo em caixas enormes
 $config['check_all_folders'] = true;
-
- // Se tiver plugins que buscam conteúdo externo (URL), dê um timeout global:
- $config['http_client'] = ['timeout' => 10]; // via Guzzle
- $config['enable_spellcheck'] = false;
- #$config['imap_conn_options'] =  array('verify_peer' => false, 'verify_peer_name' => false) ;
- #$config['smtp_conn_options'] =  array('verify_peer' => false, 'verify_peer_name' => false) ;
- $config['imap_conn_options'] = array(
-  'ssl'         => array(
-     'verify_peer'  => false,
-     'verify_peer_name'  => false
-   )
- );
-
- $config['smtp_conn_options'] = array(
-  'ssl'         => array(
-     'verify_peer'  => false,
-     'verify_peer_name'  => false
-   )
- );
- $config['max_message_size'] = 70000000;  // ~70MB para garantir 50MB de anexos
- $config['log_driver'] = 'file';
- $config['log_driver'] = 'stdout';
- $config['log_logins'] = true;  // Registra logins
- $config['language'] = 'pt_BR';
- $config['htmleditor'] = 1;
- $config['reply_mode'] = 1;
- $config['session_lifetime'] = 2880;
- $config['reconnect_imap_max_attempts'] = 3;
  #$config['password_driver'] = 'sql';
  #$config['password_confirm_current'] = true;
  #$config['password_minimum_length'] = 5;
